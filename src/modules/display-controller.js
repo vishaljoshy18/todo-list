c559c789b883c5a7c554de06929f567ed6aec016
VishalJoshy <1812-vishal@protonmail.com>
2021-07-28 21:17:34 +0530

    Adds add-new-project form

--- src/modules/display-controller.js
+++ src/modules/display-controller.js
@@ -9,18 +9,58 @@ const sidenav = (function () {
         projectListHeading.textContent = 'Projects';
         sidenav.appendChild(projectListHeading);
 
-        const addProjectButton = document.createElement('button');
-        addProjectButton.textContent = 'Add Project';
-        addProjectButton.setAttribute('id', 'add-project-button');
-        sidenav.appendChild(addProjectButton);
+        const openProjectFormButton = document.createElement('button');
+        openProjectFormButton.textContent = 'Add Project';
+        openProjectFormButton.setAttribute('id', 'open-project-form');
+        sidenav.appendChild(openProjectFormButton);
 
         const projectList = document.createElement('div');
         projectList.setAttribute('id', 'project-list');
         sidenav.appendChild(projectList);
 
+        sidenav.appendChild(createProjectForm());
+
         document.body.appendChild(sidenav);
 
         updateProjectList();
+    };
+    const createProjectForm = function () {
+        const popupForm = document.createElement('div');
+        popupForm.setAttribute('class', 'form-popup');
+
+        const form = document.createElement('form');
+        form.setAttribute('id', 'project-form');
+
+        const formHeader = document.createElement('h2');
+        formHeader.textContent = 'Add Project';
+        const label = document.createElement('label');
+        label.textContent = 'Project Name';
+        label.setAttribute('for', 'project-name');
+        const input = document.createElement('input');
+        input.setAttribute('name', 'project-name');
+        input.setAttribute('id', 'project-name');
+        input.setAttribute('placeholder', 'Enter project name');
+        input.setAttribute('type', 'text');
+        input.setAttribute('required', true);
+
+        const addButton = document.createElement('button');
+        addButton.setAttribute('type', 'submit');
+        addButton.setAttribute('id', 'add-project-button');
+        addButton.textContent = 'Add Project';
+        const cancelButton = document.createElement('button');
+        cancelButton.setAttribute('type', 'submit');
+        cancelButton.setAttribute('id', 'cancel-button');
+        cancelButton.textContent = 'Cancel';
+
+        form.appendChild(formHeader);
+        form.appendChild(label);
+        form.appendChild(input);
+        form.appendChild(addButton);
+        form.appendChild(cancelButton);
+
+        popupForm.appendChild(form);
+
+        return popupForm;
     };
 
     const updateProjectList = function () {
@@ -37,13 +77,19 @@ const sidenav = (function () {
 })();
 
 const eventHandler = (function () {
-    const addProjectEventListner = function () {
-        const addProjectButton = document.querySelector('#add-project-button');
-        addProjectButton.addEventListener('click', openProjectForm);
+    const addProjectEventListener = function () {
+        const openProjectFormButton = document.querySelector('#open-project-form');
+        openProjectFormButton.addEventListener('click', openProjectForm);
     };
 
     const openProjectForm = function () {
-        addProject('default');
+        document.querySelector('.form-popup').style.display = 'block';
+        const form = document.querySelector('#project-form');
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            console.log('form works');
+        });
     };
 
     const addProject = function (projectName) {
@@ -51,12 +97,12 @@ const eventHandler = (function () {
         sidenav.updateProjectList();
     };
 
-    return { addProjectEventListner };
+    return { addProjectEventListener };
 })();
 
 const loadSideNav = function () {
     sidenav.createNav();
-    eventHandler.addProjectEventListner();
+    eventHandler.addProjectEventListener();
 };
 
 export { loadSideNav };
