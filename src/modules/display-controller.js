40da0dac059ce96ea52ac5a1829babc15706b0a7
VishalJoshy <1812-vishal@protonmail.com>
2021-07-28 15:42:28 +0530

    Adds button to add new projects.Display updated project list on sidenav.

--- src/modules/display-controller.js
+++ src/modules/display-controller.js
@@ -1,32 +1,62 @@
-import { projects } from './project';
+import { createProject, getProjects } from './project';
 
-const createSideNav = function () {
-    const sidenav = document.createElement('div');
-    sidenav.setAttribute('class', 'side-nav');
-    document.body.appendChild(sidenav);
-    
-    loadProjectList();
-};
+const sidenav = (function () {
+    const createNav = function () {
+        const sidenav = document.createElement('div');
+        sidenav.setAttribute('class', 'side-nav');
 
-const loadProjectList = function () {
-    const sidenav = document.querySelector('.side-nav');
-    getProjectDivList().forEach((project) => {
-        sidenav.appendChild(project);
-    });
-};
+        const projectListHeading = document.createElement('h3');
+        projectListHeading.textContent = 'Projects';
+        sidenav.appendChild(projectListHeading);
 
-const getProjectDivList = function () {
-    let divList = [];
-    projects.forEach((project) => {
-        const div = document.createElement('div');
-        div.textContent = project.projectName;
-        divList.push(div);
-    });
-    return divList;
-};
+        const addProjectButton = document.createElement('button');
+        addProjectButton.textContent = 'Add Project';
+        addProjectButton.setAttribute('id', 'add-project-button');
+        sidenav.appendChild(addProjectButton);
+
+        const projectList = document.createElement('div');
+        projectList.setAttribute('id', 'project-list');
+        sidenav.appendChild(projectList);
+
+        document.body.appendChild(sidenav);
+
+        updateProjectList();
+    };
+
+    const updateProjectList = function () {
+        const projectList = document.querySelector('#project-list');
+        projectList.innerHTML = '';
+        getProjects().forEach((project) => {
+            const div = document.createElement('div');
+            div.textContent = project.projectName;
+            projectList.appendChild(div);
+        });
+    };
+
+    return { createNav, updateProjectList };
+})();
+
+const eventHandler = (function () {
+    const addProjectEventListner = function () {
+        const addProjectButton = document.querySelector('#add-project-button');
+        addProjectButton.addEventListener('click', openProjectForm);
+    };
+
+    const openProjectForm = function () {
+        addProject('default');
+    };
+
+    const addProject = function (projectName) {
+        createProject(projectName);
+        sidenav.updateProjectList();
+    };
+
+    return { addProjectEventListner };
+})();
 
 const loadSideNav = function () {
-    createSideNav();
+    sidenav.createNav();
+    eventHandler.addProjectEventListner();
 };
 
 export { loadSideNav };
--- src/modules/project.js
+++ src/modules/project.js
@@ -1,22 +1,17 @@
-function createProject(name) {
+let projects = [];
+
+function projectFactory(name) {
     return { projectName: name, todoList: [] };
 }
 
-function createTodo(name, date, note) {
-    return { todoTitle: name, todoDate: date, todoNote: note };
-}
+const createProject = function (projectName) {
+    const newProject = projectFactory(projectName);
+    projects.push(newProject);
+    console.log(projects);
+};
 
-let projects = [];
-const project1 = createProject('Default');
-const project2 = createProject('Project1');
-const todo1 = createTodo('do something', 12, 'hi im back');
-const todo2 = createTodo('do something', 12, 'hi im back');
+const getProjects = function () {
+    return projects;
+};
 
-project1.todoList.push(todo1);
-
-project1.todoList.push(todo2);
-console.log(project1);
-projects.push(project1);
-projects.push(project2);
-
-export { projects };
+export { createProject, getProjects };
